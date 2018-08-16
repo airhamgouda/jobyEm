a268e9ca48f81ea4f6d578f143195553f81cfefa
airhamgouda <36106422+airhamgouda@users.noreply.github.com>
2018-08-15 18:20:29 -0600

    Update map.py

--- map.py
+++ map.py
@@ -18,10 +18,10 @@ HEIGHT = int(LENGTH / GRIDSIZE)
 
 
 
-topLeft = [0, 0]
+topLeft = [(0 - GRIDSIZE), 0]
 bottomRight = [0, 0]
 for i in testMap:
-  
+
   # Top Left XY Coordinates
   if topLeft[0] == (LENGTH - GRIDSIZE):
       topLeft[0] = 0 # Reset X coordinates to 0
